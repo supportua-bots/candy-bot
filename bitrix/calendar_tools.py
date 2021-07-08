fb945f3a55924f4bce894dc7062585318d27a8a4
Nikolai <kirichk@gmail.com>
2021-07-08 08:09:28 +0300

    check

--- bitrix/calendar_tools.py
+++ bitrix/calendar_tools.py
@@ -85,7 +85,6 @@ def chat_availability_check():
         date_end = float(event['DATE_TO_TS_UTC']) + 10800.0
         # ts = datetime.now().timestamp()
         ts = 1625741899
-        logger.info(f'{ts - date_start}, {date_end - ts}, {event["NAME"]}')
         if date_start < ts < date_end and (event['NAME'] == non_working_hours or event['NAME'] == dayoff) and date_end <= 2000000000:
             logger.info(f'{ts - date_start}, {date_end - ts}, {event["NAME"]}')
             return False
